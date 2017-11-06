a1994f6f894d5c8491e5968723b1402f59e90520
WATERTRAX\swang <swang@watertrax.com>
2017-11-06 08:19:21 -0800

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+		ILogger _logger; 
+		public ValuesController(ILogger<ValuesController> logger)
+		{
+			this._logger = logger;
+		}
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+			_logger.LogInformation("LoggingEvents.GetItem", "Getting item {ID}", id);
+			try
+			{
+
+				if (id == 100)
+				{
+					int i = id / 0;
+
+				}
+			}
+			catch(Exception e)
+			{
+				_logger.LogError(e, e.Message);
+				throw; 
+			}
+
+			return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+	public class Course
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int CourseID { get; set; }
+		public string Title { get; set; }
+		public int Credits { get; set; }
+
+		public ICollection<Enrollment> Enrollments { get; set; }
+	}
+
+	public enum Grade
+	{
+		A, B, C, D, F
+	}
+
+	public class Enrollment
+	{
+		public int EnrollmentID { get; set; }
+		public int CourseID { get; set; }
+		public int StudentID { get; set; }
+		public Grade? Grade { get; set; }
+
+		public Course Course { get; set; }
+		public Student Student { get; set; }
+	}
+
+	public class Student
+	{
+		public int ID { get; set; }
+		public string LastName { get; set; }
+		public string FirstMidName { get; set; }
+		public DateTime EnrollmentDate { get; set; }
+
+		public ICollection<Enrollment> Enrollments { get; set; }
+	}
+
+	public class SchoolContext : DbContext
+	{
+		public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Course> Courses { get; set; }
+		public DbSet<Enrollment> Enrollments { get; set; }
+		public DbSet<Student> Students { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Course>().ToTable("Course");
+			modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+			modelBuilder.Entity<Student>().ToTable("Student");
+		}
+	}
+}
